6e1d809894159616e144190f792dc96a5a1b4be3
Neo Ighodaro <neo@creativitykills.co>
2016-07-02 07:21:06 +0100

    Manipulated the migrations.

--- database/migrations/2016_06_24_000000_create_bukas_table.php
+++ database/migrations/2016_06_24_000000_create_bukas_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class Bukas extends Migration
+class CreateBukasTable extends Migration
 {
     /**
      * Run the migrations.
