03308fccc8495f3b4ac3531a613088b9108a0688
Neo Ighodaro <neo@creativitykills.co>
2016-08-21 14:27:13 +0100

    Minor fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>@yield('title')</title>
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:100,300,400,700|Montserrat:700|Source+Sans+Pro:300,400">
-    <link rel="shortcut icon" href="http://mediacdn.hotels.ng/hotels/v52/img/favicon.png">
+    <link rel="shortcut icon" href="//mediacdn.hotels.ng/hotels/v52/img/favicon.png">
     <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
     <link rel="stylesheet" href="//cdn.jsdelivr.net/bootstrap.block-grid/latest/bootstrap3-block-grid.min.css">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
