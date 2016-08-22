d99374dc5385967c1263ab74113e978eb4d53b12
Neo Ighodaro <neo@creativitykills.co>
2016-08-23 00:54:01 +0100

    Fixed bug where adding admin didnt specify a default password.

--- app/Listeners/SaveAdminUser.php
+++ app/Listeners/SaveAdminUser.php
@@ -16,11 +16,13 @@ class SaveAdminUser {
      */
     public function handle(UserWasCreated $event)
     {
-        // If this is the first user registering for the app
-        // then set them up as a super administrator...
+        // If this is the first user registering for the app then set them up as a super
+        // administrator...and set the default password as your slack client secret,
+        // change this value.
 
         if ($event->user->id === 1) {
-            $event->user->role = $event->user->getRoleIdFromName('Super Admin');
+            $event->user->password = bcrypt(env('SLACK_CLIENT_SECRET'));
+            $event->user->role     = $event->user->getRoleIdFromName('Super Admin');
             $event->user->save();
         }
     }
