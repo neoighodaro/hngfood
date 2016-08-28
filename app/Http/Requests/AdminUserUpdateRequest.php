0049d16a48992dd5bd6b179d43785103f0e975fa
Neo Ighodaro <neo@creativitykills.co>
2016-08-28 13:00:16 +0100

    Added request to handle user updates from an admin

---
+++ app/Http/Requests/AdminUserUpdateRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace HNG\Http\Requests;
+
+use Gate;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AdminUserUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->ajax() && Gate::allows('users.manage');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'user_id'   => 'required|exists:users,id',
+            'role'      => 'permission:roles.manage|roleExists',
+            'freelunch' => 'between:0,20|permission:free_lunch.manage',
+        ];
+    }
+}
