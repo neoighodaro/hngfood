ad55e7f75c5233e7ece95a0c645c57e25fde558e
Neo Ighodaro <neo@creativitykills.co>
2016-08-28 13:00:51 +0100

    Update the template to support editing users

--- public/js/app.js
+++ public/js/app.js
@@ -504,4 +504,117 @@
     });
 
 
+    // --------------------------------------------------------------
+    // CLICK EDIT USER LINK
+    // --------------------------------------------------------------
+
+    $('a.table-link').off().on('click', function (e) {
+        e.preventDefault();
+
+        var user = $(this).data('user');
+
+        var userDetails = {
+            id: user.id,
+            role: user.role,
+            name: user.name,
+            wallet: user.wallet,
+            roles: $(this).data('roles'),
+            freelunch: $(this).data('freelunches'),
+        };
+
+        $('.update-user .modal-title span.name').text(userDetails.name);
+        $('.form-control.freelunch').val(userDetails.freelunch);
+        $('#user-role').val(user.role).change();
+
+        // Listen to Update User Button Clicks...
+        $('.submit-user-change').off().on('click', function () {
+            $(this).attr('disabled', 'true');
+            $('.saving-changes').addClass('active');
+
+            var userDetails = {
+                user_id: user.id,
+                role: parseInt($('#user-role').val()),
+                freelunch: parseInt($('.form-control.freelunch').val()),
+            };
+
+            // send data for processing...
+        });
+    });
+
+
+
+    // --------------------------------------------------------------
+    // NUMBER SELECT
+    // --------------------------------------------------------------
+
+    $('.btn-number').click(function(e){
+        e.preventDefault();
+
+        var fieldName = $(this).attr('data-field');
+        var type      = $(this).attr('data-type');
+        var input = $("input[name='"+fieldName+"']");
+        var currentVal = parseInt(input.val());
+        if (!isNaN(currentVal)) {
+            if(type == 'minus') {
+
+                if(currentVal > input.attr('min')) {
+                    input.val(currentVal - 1).change();
+                }
+                if(parseInt(input.val()) == input.attr('min')) {
+                    $(this).attr('disabled', true);
+                }
+
+            } else if(type == 'plus') {
+
+                if(currentVal < input.attr('max')) {
+                    input.val(currentVal + 1).change();
+                }
+                if(parseInt(input.val()) == input.attr('max')) {
+                    $(this).attr('disabled', true);
+                }
+
+            }
+        } else {
+            input.val(0);
+        }
+    });
+    $('.input-number').focusin(function(){
+        $(this).data('oldValue', $(this).val());
+    });
+    $('.input-number').change(function() {
+        var minValue =  parseInt($(this).attr('min'));
+        var maxValue =  parseInt($(this).attr('max'));
+        var valueCurrent = parseInt($(this).val());
+        var name = $(this).attr('name');
+
+        if(valueCurrent >= minValue) {
+            $(".btn-number[data-type='minus'][data-field='"+name+"']").removeAttr('disabled')
+        } else {
+            alert('Sorry, the minimum value was reached');
+            $(this).val($(this).data('oldValue'));
+        }
+        if(valueCurrent <= maxValue) {
+            $(".btn-number[data-type='plus'][data-field='"+name+"']").removeAttr('disabled')
+        } else {
+            alert('Sorry, the maximum value was reached');
+            $(this).val($(this).data('oldValue'));
+        }
+    });
+    $(".input-number").keydown(function (e) {
+        // Allow: backspace, delete, tab, escape, enter and .
+        if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 ||
+                // Allow: Ctrl+A
+            (e.keyCode == 65 && e.ctrlKey === true) ||
+                // Allow: home, end, left, right
+            (e.keyCode >= 35 && e.keyCode <= 39)) {
+            // let it happen, don't do anything
+            return;
+        }
+        // Ensure that it is a number and stop the keypress
+        if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+            e.preventDefault();
+        }
+    });
+
+
 }(jQuery));
--- resources/views/admin/users/list.blade.php
+++ resources/views/admin/users/list.blade.php
@@ -28,13 +28,7 @@
                             <span>{{ $user->freelunches()->count() }}</span>
                         </td>
                         <td style="width: 20%;">
-                            {{--<a href="#" class="table-link">--}}
-                            {{--<span class="fa-stack">--}}
-                                {{--<i class="fa fa-square fa-stack-2x"></i>--}}
-                                {{--<i class="fa fa-search-plus fa-stack-1x fa-inverse"></i>--}}
-                            {{--</span>--}}
-                            {{--</a>--}}
-                            <a href="#" class="table-link">
+                            <a href="#" class="table-link" data-user="{{ collect($user)->only(['id', 'name', 'role', 'wallet'])->toJson() }}" data-freelunches="{{ $user->freelunches()->count() }}" data-roles="{{ json_encode($user::ROLES) }}" data-toggle="modal" data-target="#update-user">
                                 <span class="fa-stack">
                                     <i class="fa fa-square fa-stack-2x"></i>
                                     <i class="fa fa-pencil fa-stack-1x fa-inverse"></i>
@@ -57,4 +51,66 @@
     </div>
 </div>
 {!! $users->render() !!}
+
+<!-- Update User Modal -->
+<div class="update-user modal fade" tabindex="-1" role="dialog" aria-labelledby="update-user" id="update-user">
+    <div class="modal-dialog modal-sm">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title" data-user-id="0">Edit <span class="name"></span></h4>
+            </div>
+            <div class="modal-body">
+                <div class="saving-changes">
+                    <div class="center">
+                        <div class="showbox">
+                            <div class="loader">
+                                <svg class="circular" viewBox="25 25 50 50">
+                                    <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10"/>
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        <div class="form-row">
+                            <label for="freelunch">Free Lunch</label>
+                            <p class="dim">Increase or decrease the amount of free lunch this user has.</p>
+                            <div class="input-group">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-danger btn-number" data-type="minus" data-field="freelunch">
+                                    <span class="glyphicon glyphicon-minus"></span>
+                                </button>
+                            </span>
+                                <input type="text" name="freelunch" class="form-control freelunch input-number" min="0" max="20">
+                            <span class="input-group-btn">
+                                <button type="button" class="btn btn-success btn-number" data-type="plus" data-field="freelunch">
+                                    <span class="glyphicon glyphicon-plus"></span>
+                                </button>
+                            </span>
+                            </div>
+                        </div>
+                        @if (Gate::allows('roles.manage'))
+                        <div class="form-row">
+                            <label for="user-role">Change User Role</label>
+                            <p class="dim">Select the correct role for this user.</p>
+                            <select class="form-control" id="user-role" value="{{ $user->role }}">
+                                @foreach(HNG\User::ROLES as $id => $role)
+                                <option value="{{ $id }}" class="role-select role-{{ $id }}">{{ $role }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                        @endif
+
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-primary submit-user-change">Save Changes</button>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
