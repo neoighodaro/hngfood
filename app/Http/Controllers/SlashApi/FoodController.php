eb74f121e9e3398444374fd7a26bd68803214935
Neo Ighodaro <neo@creativitykills.co>
2016-08-30 21:16:39 +0100

    Nothing.

--- app/Http/Controllers/SlashApi/FoodController.php
+++ app/Http/Controllers/SlashApi/FoodController.php
@@ -5,8 +5,7 @@ namespace HNG\Http\Controllers\SlashApi;
 use HNG\Http\Controllers\Controller;
 
 class FoodController extends Controller {
-
-
+    
     public function index()
     {
         return [];
