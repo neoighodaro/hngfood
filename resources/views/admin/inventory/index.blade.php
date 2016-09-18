4bf2684391176d41c14d5c136f2b018942711604
Neo Ighodaro <neo@creativitykills.co>
2016-09-18 12:28:09 +0100

    Added styling for the bukas list

--- resources/views/admin/inventory/index.blade.php
+++ resources/views/admin/inventory/index.blade.php
@@ -5,14 +5,15 @@
 @section('admin-content')
     <div class="stats-section clearfix">
         <h3 class="pull-left">Available Bukas</h3>
-        <a class="btn btn-primary pull-right" style="position:relative;top:-8px;">Add Buka</a>
+        <a class="btn btn-success pull-right add-food-btn">Add Food Item to Buka</a>
+        <a class="btn btn-primary pull-right add-buka-btn">Add Buka</a>
     </div>
     <div class="row">
         <div class="col-sm-12">
-            <ul>
+            <ul class="bukas-list">
             @foreach($bukas as $buka)
                 <li>
-                    <div class="buka-wrapper">
+                    <div class="buka-wrapper" style="background-image: url('{{ $buka->avatar }}');">
                         <h4>{{ $buka->name }}</h4>
                         <span>{{ $buka->lunches->count() }} Food Type(s)</span>
                         <div class="overlay"></div>
