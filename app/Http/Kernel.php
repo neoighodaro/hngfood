2a420063f812519596dbd4dc4bc146980577d644
jolaolu <adebayoanjola@gmail.com>
2018-12-30 01:09:01 +0100

    fixed middleware bug

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $middleware = [
-        \Fideloper\Proxy\TrustProxies::class,
+        \HNG\Http\Middleware\TrustProxies::class,
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
     ];
 
